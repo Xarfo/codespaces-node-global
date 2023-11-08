ca25d2353e142a6de55c0ac4a601a4f8cf114529
Abdul Ahmad <abdul.ahmad.dev@gmail.com>
2023-11-08 00:23:34 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var message = ''
+console.log('-------------------------------------------------')
+console.log(global)
+console.log('-------------------------------------------------')
+console.log(global.message)
+console.log('-------------------------------------------------')
+console.log(message)
+console.log('-------------------------------------------------')
+
+//   glabal onject of Node.js is global same as the window object. Variables declared are only scoped to the module and not attached the global object.
+//   clearInterval: [Function: clearInterval],
+//   clearTimeout: [Function: clearTimeout],
+//   setInterval: [Function: setInterval],
+//   setTimeout: [Function: setTimeout] 
